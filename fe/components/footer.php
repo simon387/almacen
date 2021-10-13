d6ee5ebd7173deaf2ba612696cd8594279a72562
export GIT_AUTHOR_EMAIL= <simon387@hotmail.it>
2021-10-13 10:52:48 +0200

    rm_index_template_page

--- fe/components/footer.php
+++ fe/components/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
 	<div class="container my-auto">
 		<div class="copyright text-center my-auto">
-			<span>ALMACEN VERSION: 1.3.4 - Copyright &copy; www.simonecelia.it 2021</span>
+			<span>ALMACEN VERSION: 1.3.5 - Copyright &copy; www.simonecelia.it 2021</span>
 		</div>
 	</div>
 </footer>
