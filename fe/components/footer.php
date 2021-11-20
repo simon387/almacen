c072d8e59f2d1027b68687ae9db6647badeeface
simone <simon387@hotmail.it>
2021-11-20 22:55:34 +0100

    fix_footer

--- fe/components/footer.php
+++ fe/components/footer.php
@@ -9,7 +9,7 @@ fclose($f);
 <footer class="sticky-footer bg-white">
 	<div class="container my-auto">
 		<div class="copyright text-center my-auto">
-			<span>ALMACEN <?php echo $version ?> - Copyright &copy; www.simonecelia.it 2021</span>
+            <span>ALMACEN <?php echo $version ?> - Copyright &copy; <a target="_blank" href="https:\\www.simonecelia.it">www.simonecelia.it</a> 2021</span>
 		</div>
 	</div>
 </footer>
